3f2d9940dbd92e6df7f8b7ddcaae6a5e51eb4c85
Joao Vitor <joao.vitordeoliveira@hotmail.com>
2022-02-23 15:49:12 -0300

    Documentado com Swagger

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -13,7 +13,7 @@ class CreateUserUseCase {
     const userAlreadyExist = this.usersRepository.findByEmail(email);
 
     if (userAlreadyExist) {
-      throw new Error("User already exist!");
+      throw new Error("This email is already in use.");
     }
 
     const user = this.usersRepository.create({ name, email });
